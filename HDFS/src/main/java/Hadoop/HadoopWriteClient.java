f5476606cb5cc0ccb9a2c2e21ebf08ebb2eb4f93
H. Kardos <hkardos@umich.edu>
2017-08-15 20:37:20 -0400

    Chasing weird bug

--- HDFS/src/main/java/Hadoop/HadoopWriteClient.java
+++ HDFS/src/main/java/Hadoop/HadoopWriteClient.java
@@ -275,6 +275,7 @@ public class HadoopWriteClient{
             files.add(listOfFiles[i].toString());
         }
         // Removes local files
+        /*
         for(int i = 0; i < files.size(); ++i){
             String removeLocal = "rm " + files.get(i);
             try{
@@ -284,7 +285,7 @@ public class HadoopWriteClient{
                 e.printStackTrace();
             }
         }
-
+        */
     }
 
 
