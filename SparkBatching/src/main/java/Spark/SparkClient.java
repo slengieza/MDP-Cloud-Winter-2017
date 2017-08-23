3f13ebb124432b4e7d5c76ed4c88886781134f47
H. Kardos <hkardos@umich.edu>
2017-08-22 20:07:22 -0400

    Testing

--- SparkBatching/src/main/java/Spark/SparkClient.java
+++ SparkBatching/src/main/java/Spark/SparkClient.java
@@ -13,7 +13,6 @@ import org.json.JSONObject;
 
 public class SparkClient {
     public static void main(String[] args) {
-        /*String logFile = "hdfs:///var/mdp-cloud/";
         System.out.println("Please Enter Which File To Work On: ");
         try{
             Process display = Runtime.getRuntime().exec("hdfs dfs -ls /var/mdp-cloud/");
@@ -21,11 +20,14 @@ public class SparkClient {
         }
         catch(Exception e){
             e.printStackTrace();
-        }*/
+        }
+        Scanner scans = new Scanner();
+        String fileIn;
+        fileIn = scans.nextLine()
         SparkConf conf = new SparkConf().setAppName("Spark Client");
         JavaSparkContext sc = new JavaSparkContext(conf);
         SQLContext sqlContext = new org.apache.spark.sql.SQLContext(sc);
-        DataFrame df = sqlContext.read().json("hdfs:///var/mdp-cloud/test1.txt");
+        DataFrame df = sqlContext.read().json("hdfs:///var/mdp-cloud/" + );
         df.show();
     }
 }
