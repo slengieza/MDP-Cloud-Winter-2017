947ebaf21c0e0bc49162f4595a91071285c13ffd
H. Kardos <hkardos@umich.edu>
2017-08-28 14:20:14 -0400

    Testing

--- Producer/src/main/java/JsonToXML.java
+++ Producer/src/main/java/JsonToXML.java
@@ -33,14 +33,14 @@ import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
- 
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
 public class JsonToXML {
 
-	
+
 	public static void GetMTCXML(File jsonfile) {
 		// Initialize this array to check later
 		JsonArray array = null;
@@ -63,42 +63,42 @@ public class JsonToXML {
 			Document doc = docBuilder.newDocument();
 			Element rootElement = doc.createElement("MTConnectStreams");
 			doc.appendChild(rootElement);
-			
+
 			Element streams = doc.createElement("Streams");
 			rootElement.appendChild(streams);
-			
+
 			//FANUC loop setup
 			Element deviceStream = doc.createElement("DeviceStream");
 			streams.appendChild(deviceStream);
 			deviceStream.setAttribute("name", "FANUC");
-			
+
 			Element samples = doc.createElement("Samples");
 			deviceStream.appendChild(samples);
-			
+
 			//Element current = doc.createElement("Current");
 			//Element freq = doc.createElement("Freq");
 			//Element volt = doc.createElement("Volt");
 			//samples.appendChild(current);
 			//samples.appendChild(freq);
 			//samples.appendChild(volt);
-			
+
 			//ABB loop setup
 			Element deviceStream2 = doc.createElement("DeviceStream");
 			streams.appendChild(deviceStream2);
 			deviceStream2.setAttribute("name", "ABB");
-			
+
 			Element samples2 = doc.createElement("Samples");
 			deviceStream2.appendChild(samples2);
-			
+
 			//Element current2 = doc.createElement("Current");
 			//Element freq2 = doc.createElement("Freq");
 			//Element volt2 = doc.createElement("Volt");
 			//samples2.appendChild(current2);
 			//samples2.appendChild(freq2);
 			//samples2.appendChild(volt2);
-			
 
-		} 
+
+		}
 		catch (Exception e) {}
 
 		// This happens when program fails to open file
@@ -155,18 +155,18 @@ public class JsonToXML {
 						 a.setAttribute("is",tagName);
 						 freq.appendChild(doc.createTextNode(tagValue));
 						 samples.appendChild(freq);
-						 
+
 			}
 			timeStamp = Long.parseLong(object.getString("TimeStamp").substring(6,19));
 			x++;
 		}
 		streams.setAttribute("TimeStamp",timeStamp);
-		
+
 		//output file to place
 		TransformerFactory transformerFactory = TransformerFactory.newInstance();
 		Transformer transformer = transformerFactory.newTransformer();
 		DOMSource source = new DOMSource(doc);
 		//change place
-		StreamResult result = new StreamResult(new File("C:\Rockwell Automation\WorkingDirectory\MTConnect\devices.xml"));
+		StreamResult result = new StreamResult(new File("C:\\Rockwell Automation\\WorkingDirectory\\MTConnect\\devices.xml"));
    }
 }
